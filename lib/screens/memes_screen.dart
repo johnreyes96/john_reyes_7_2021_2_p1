3626fd4a4e77ecb8f85e4cdc02a9a9597b24cb27
johnfreyes <jhonfer96@hotmail.com>
2021-10-23 11:34:23 -0500

    initial memes screen

---
+++ lib/screens/memes_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MemesScreen extends StatefulWidget {
+  const MemesScreen({ Key? key }) : super(key: key);
+
+  @override
+  _MemesScreenState createState() => _MemesScreenState();
+}
+
+class _MemesScreenState extends State<MemesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Memes Screen');
+  }
+}
